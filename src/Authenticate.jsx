bbd2ee97add05d65e1675a477c49b4f301161ac8
SriramAnanth2912 <sriramananth2912@gmail.com>
2024-03-24 23:34:29 +0530

    Added context provider for easy access of user token across all components

--- src/Authenticate.jsx
+++ src/Authenticate.jsx
@@ -2,7 +2,6 @@
 import { createContext, useState } from "react";
 
 export const Authorise = createContext();
-
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState("");
   return <Authorise.Provider value={{ token, setToken }}>{children}</Authorise.Provider>;
