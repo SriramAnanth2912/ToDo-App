2b3180b28a2e65892cbbe4ab106a191926d50a76
SriramAnanth2912 <sriramananth2912@gmail.com>
2024-03-24 23:31:25 +0530

    created complete-component and implemented completed tasks section in todo-page

---
+++ src/components/CompleteList.jsx
@@ -0,0 +1,44 @@
+import styles from "../tasks.styles.module.css";
+import PropTypes from "prop-types";
+
+function Complete({ list, deletetask }) {
+  if (!list || list.length === 0) {
+    return <div>No completed todos found</div>;
+  }
+
+  return (
+    <div className={styles.completedTasks}>
+      <ul>
+        {list.map((todo, index) =>
+          todo.is_complete ? (
+            <li key={index} className={styles.todoItem}>
+              <span className={styles.index}>Task-{index + 1}: </span>
+              <span className={styles.updatetask}> {todo.task}</span>
+              <span
+                onClick={(e) => {
+                  e.preventDefault();
+                  deletetask(todo.id);
+                }}
+                className={styles.actionIcon}
+              >
+                &times;
+              </span>{" "}
+            </li>
+          ) : null
+        )}
+      </ul>
+    </div>
+  );
+}
+
+Complete.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      task: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  deletetask: PropTypes.func.isRequired,
+};
+
+export default Complete;
